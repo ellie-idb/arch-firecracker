e829dce3c3f839afff0388521b86bf735536bee1
hatf0 <harrison@0xcc.pw>
2019-08-25 19:00:53 -0400

    Ignore any linux kernel updates

--- script/provision.sh
+++ script/provision.sh
@@ -8,6 +8,7 @@ pacman --noconfirm -S dmd
 echo 'arch-firecracker' > /etc/hostname
 passwd -d root
 mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
+echo 'IgnorePkg = linux' >> /etc/pacman.conf
 cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
 [Service]
 ExecStart=
