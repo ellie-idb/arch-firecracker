21bda8c072abb98e70f40ee3d0001dfeffd6acfc
Harrison Ford <harrison@0xcc.pw>
2020-07-28 15:59:38 -0400

    general: update provision script to look for .zst files

--- script/provision.sh
+++ script/provision.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 set -ex
 
-pacman --noconfirm -U /mnt/home/build/linux/trunk/linux*.tar.xz
+pacman --noconfirm -U /mnt/home/build/linux/trunk/linux*.pkg.tar.zst
+
+# add custom packages here if you want them, dmd is included
+# since it's an integral part of *my* infrastructure
 
 pacman --noconfirm -S dmd
 
 echo 'arch-firecracker' > /etc/hostname
 passwd -d root
 mkdir /etc/systemd/system/serial-getty@ttyS0.service.d/
-echo 'IgnorePkg = linux' >> /etc/pacman.conf
+sed -i 's/#IgnorePkg\W*=/IgnorePkg = linux/' /etc/pacman.conf
 cat <<EOF > /etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
 [Service]
 ExecStart=
